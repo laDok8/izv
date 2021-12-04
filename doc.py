63769932a0946aba57ec3d182447072fff951173
Ladislav Dokoupil <ldokoupi@redhat.com>
2021-12-04 20:02:40 +0100

    wip

---
+++ doc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.8
+# coding=utf-8
+import pandas as pd
+import geopandas
+import matplotlib.pyplot as plt
+import contextily as ctx
+import sklearn.cluster
+import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
