19eb9e63823707990430a48bcaab0bda8023977f
Ladislav Dokoupil <ldokoupi@redhat.com>
2022-01-01 19:52:37 +0100

    final

--- doc.py
+++ doc.py
@@ -1,9 +1,79 @@
 #!/usr/bin/python3.8
 # coding=utf-8
 import pandas as pd
-import geopandas
 import matplotlib.pyplot as plt
-import contextily as ctx
-import sklearn.cluster
-import numpy as np
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+import seaborn as sns
+
+
+def plot(filename: str = "accidents.pkl.gz", fig_location: str = "fig.png") -> pd.DataFrame:
+    """
+    Plot alcohol based accidents information
+    :param filename: name of file with data
+    :param fig_location: name of file to store plot
+    """
+    df = pd.read_pickle(filename)[['p11', 'p6', 'region', 'p2a']]
+    before = df.shape[0]
+    print('datovy rozsah:', df['p2a'].min(), ' -- ', df['p2a'].max())
+
+    # pouze s alkoholem a odstraneni neznamych druhu nehod
+    df = df.loc[~df['p11'].isin([2, 0])]
+    print('nehod pod vlivem latek:', df.shape[0])
+    print(f'podil daných nehod z celku: {df.shape[0] * 100 / before:2.2f} %')
+    df = df.loc[~df['p6'].isin([0])]
+    print('dobře zformovaná data:', df.shape[0])
+
+    # druh nehody
+    df["p6"] = df["p6"].replace({
+        1: "vozidlo",
+        2: 'statická překážka',
+        3: 'statická překážka',
+        4: "živé objekty",
+        5: "živé objekty",
+        6: "živé objekty",
+        7: "vozidlo",
+        8: "vozidlo",
+        9: "havárie"
+    })
+    df["p11"] = df["p11"].replace({
+        1: "<0,24 ‰",
+        3: "0,24 ‰ - 0,5 ‰",
+        4: "drogy",
+        5: "alkohol + drogy",
+        6: "0,5 ‰ - 0,8 ‰",
+        7: "0,8 ‰ - 1,0 ‰",
+        8: "1,0 ‰ - 1,5 ‰",
+        9: ">1,5 ‰"
+    })
+
+    df1 = df.groupby(['region']).agg('count').reset_index().rename(columns={'p11': 'počet nehod'})[
+        ['region', 'počet nehod']].rename(columns={'region': 'kraj'})
+    print(f'podil kraje s největším/nejmenším poctem nehod: {df1["počet nehod"].max() / df1["počet nehod"].min():2.1f}')
+
+    df2 = df.groupby(['p11', 'p6']).agg('count').reset_index().rename(
+        columns={'p6': 'druh nehody', 'p11': 'ovlivnění', 'region': 'počet nehod'})
+    nehody_alk = df2.loc[~df2["ovlivnění"].isin(["drogy"])]["počet nehod"].sum() * 100
+    print(f'nehod s alkoholem: {nehody_alk / df2["počet nehod"].sum():2.2f} %')
+
+    # plot
+    sns.set_theme(style="ticks", rc={"axes.spines.right": False, "axes.spines.top": False})
+    g = sns.catplot(kind='bar', data=df2, y='ovlivnění', x='počet nehod', hue='druh nehody')
+    g.fig.suptitle('Cíl srážky v opilosti')
+    g.legend.set_bbox_to_anchor([0.9, 0.7])
+    g.fig.tight_layout()
+    # g.fig.show()
+    plt.savefig(fig_location)
+
+    # print data
+    nehody_static = df2.loc[~df2["druh nehody"].isin(["statická překážka"])]["počet nehod"].sum() * 100
+    print(f'podil statických překážek: {nehody_static / df2["počet nehod"].sum():2.2f} %')
+    print('nehod vůči živým bytostem:', df2.loc[df2["druh nehody"].isin(["živé objekty"])]["počet nehod"].sum())
+    nehody_ovliv = df2.loc[df2["ovlivnění"].isin([">1,5 ‰"])]["počet nehod"].sum() * 100
+    print(f'podil nehod pod vlivem >1,5 ‰: {nehody_ovliv / df.shape[0]:2.2f} %')
+
+    print("\ncsv data:")
+    df1 = df1.append({'kraj': 'celkem', 'počet nehod': df1["počet nehod"].sum()}, ignore_index=True)
+    print(df1.to_csv(index=False, sep="\t"))
+
+
+if __name__ == "__main__":
+    plot()
--- geo.py
+++ geo.py
@@ -11,24 +11,68 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
-    pass
+    df['p2a'] = df['p2a'].astype('datetime64')
+    df.rename(columns={'p2a': 'date'}, inplace=True)
+    df = df.dropna(subset=['d', 'e'])
+    return geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df['d'], df['e']), crs="epsg:5514")
 
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s sesti podgrafy podle lokality nehody
      (dalnice vs prvni trida) pro roky 2018-2020 """
-    pass
+    gdf_jhm = gdf[gdf['region'] == 'JHM'].to_crs('epsg:3857')
+    fig, axs = plt.subplots(3, 2, figsize=(12, 15))
+    static = {'clr': ['green', 'red'], 'road': ['dálnice', 'silnice první třídy']}
+    for i in range(3):
+        for p in range(2):
+            axs[i, p].axis('off')
+            gdf_jhm[(gdf_jhm['p36'] == p) & (gdf_jhm['date'].dt.year == 2018 + i)].plot(ax=axs[i, p], markersize=5,
+                                                                                        color=static['clr'][p])
+            axs[i, p].title.set_text('JHM kraj: ' + static['road'][p] + " " + str(2018 + i))
+            axs[i, p].set_ylim(axs[0, 0].get_ylim())
+            axs[i, p].set_xlim(axs[0, 0].get_xlim())
+            ctx.add_basemap(axs[i, p], crs=gdf_jhm.crs.to_string(), source=ctx.providers.Stamen.TonerLite)
+            # ctx.providers.CartoDB.Positron ctx.providers.Stamen.TonerLite ctx.providers.NASAGIBS.ViirsEarthAtNight2012
+
+    fig.suptitle("Mapa nehod", fontsize=32)
+    plt.tight_layout()
+
+    if fig_location:
+        plt.savefig(fig_location)
+
+    if show_figure:
+        plt.show()
+        plt.close()
 
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
-    pass
+    gdf_jhm = gdf[(gdf['region'] == 'JHM') & (gdf['p36'] == 1)].to_crs('epsg:3857')
+    gdf_jhm = gdf_jhm.reset_index()
+    cords = np.dstack([gdf_jhm.geometry.x, gdf_jhm.geometry.y]).reshape(-1, 2)
+    gdf_jhm['cluster'] = sklearn.cluster.MiniBatchKMeans(n_clusters=25).fit(cords).labels_
+    gdf_jhm = gdf_jhm.dissolve(by='cluster', aggfunc={'p1': 'count'}).rename(columns={'p1': 'cnt'})
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+    cax = make_axes_locatable(ax).append_axes("bottom", size="5%", pad=0.1)
+    gdf_jhm.plot(ax=ax, cax=cax, column='cnt', alpha=0.5, markersize=5, legend=True,
+                 legend_kwds={'label': 'četnost', 'orientation': 'horizontal'})
+    ctx.add_basemap(ax=ax, crs=gdf_jhm.crs.to_string(), source=ctx.providers.Stamen.TonerLite)
+    ax.axis('off')
+    fig.suptitle("Mapa nehod pro silnice 1. třídy", fontsize=16)
+    plt.tight_layout()
+
+    if fig_location:
+        plt.savefig(fig_location)
+
+    if show_figure:
+        plt.show()
+        plt.close()
 
 
 if __name__ == "__main__":
-    # zde muzete delat libovolne modifikace
     gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
     plot_geo(gdf, "geo1.png", True)
     plot_cluster(gdf, "geo2.png", True)
